b16d36bfbdc41cdf1e14e9ea38748a48f0273dc2
danbrud <dannybrudner@users.noreply.github.com>
2019-07-23 13:40:33 +0300

    added CORS

--- src/server.js
+++ src/server.js
@@ -25,6 +25,14 @@ import path from 'path'
 
     // app.use( express.static( path( __dirname, 'public' ) ) )
 
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+        next()
+    })
+
     app.use( '/api/photos', photosRouter )
 
     app.listen( PORT, () => console.log( `Server is running on port ${ PORT }` ) )
